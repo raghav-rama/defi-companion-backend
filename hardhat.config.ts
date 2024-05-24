8ff5368787d6984ecc6f670f6a749921a9abf542
raghav-rama <hackerer528@gmail.com>
2024-05-24 19:46:55 +0530

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,12 @@ import "@nomicfoundation/hardhat-ignition-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  ignition: {
+    blockPollingInterval: 1_000,
+    timeBeforeBumpingFees: 3 * 60 * 1_000,
+    maxFeeBumps: 4,
+    requiredConfirmations: 5,
+  },
 };
 
 export default config;
